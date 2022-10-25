d1c7df33e7e6c081067785720a27c4ec028d68bc
pham phuc <phamvhphuc@gmail.com>
2022-10-25 11:00:42 +0700

    init

---
+++ components/footer.js
@@ -0,0 +1,7 @@
+export const Footer = () => {
+  return (
+    <div style={{ position: "absolute", bottom: 0 }}>
+      <h2>Footer</h2>
+    </div>
+  )
+};
---
+++ components/header.js
@@ -0,0 +1,14 @@
+import React from 'react'
+import { Image } from 'semantic-ui-react'
+
+export const Header = () => {
+  return (
+    <div style={{ padding: "2rem 0" }}>
+      <Image
+        src='https://www.hoanmydalat.com/upload/hoanmydalat.com/images/option/2022-02-28/1646072190_cg8UPgmomy.png'
+        size='medium'
+        style={{ width: "250px" }}  
+      />
+    </div>
+  )
+};
